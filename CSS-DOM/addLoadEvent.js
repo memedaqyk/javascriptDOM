6cbe85e25dba05aa56693bc845ac3cf6639f25dc
kange <2906261741@qq.com>
2016-07-05 23:38:21 +0800

    根据元素在节点树中的位置来设置样式

---
+++ CSS-DOM/addLoadEvent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 29062 on 2016/7/5.
+ */
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}

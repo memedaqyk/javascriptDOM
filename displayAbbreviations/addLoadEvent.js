76cf19db3a32fb499fa07aff327ab51992f87d7f
kange <2906261741@qq.com>
2016-07-05 14:08:35 +0800

    最终标记

---
+++ displayAbbreviations/addLoadEvent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 29062 on 2016/6/21.
+ */
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}

dca84626e69ae81fcaacf4ea41efd8898211166c
kange <qianyongkang12345@gamil.com>
2016-09-10 15:37:28 +0800

    js 简单动画

---
+++ positionMessage/positionMessage.js
@@ -0,0 +1,37 @@
+var movement;
+function positionMessage(){
+	if (!document.getElementById) return;
+	if (!document.getElementById('message')) return;
+	var elem = document.getElementById('message');
+	elem.style.position = 'absolute';
+	elem.style.left = '100px';
+	elem.style.top = '50px';
+	//movement = setTimeout('moveMessage()', 5000);
+}
+//clearTimeout(movement);
+function moveMessage(){
+	if (!document.getElementById) return;
+	if (!document.getElementById('message')) return;
+	var elem = document.getElementById('message');
+	var xpos = parseInt(elem.style.left);
+	var ypos = parseInt(elem.style.top);
+	if(xpos == 200 && ypos == 100){
+		return true;
+	}
+	if (xpos < 200) {
+		xpos++;
+	}
+	if (xpos > 200) {
+		xpos--;
+	}
+
+	if (ypos < 100) {
+		ypos++;
+	}
+	if (ypos > 100) {
+		ypos--;
+	}
+	elem.style.left = xpos + 'px';
+	elem.style.top = ypos + 'px';
+	movement = setTimeout('moveMessage()', 10);
+}
